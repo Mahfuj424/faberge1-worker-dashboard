4d55322ba9f4f2212973d3595b867911e0f78c14
Mahfuj424 <mahfujahmad424@gmail.com>
2025-10-21 13:28:45 +0600

    delete stripe files

--- src/lib/Stripe.ts
+++
@@ -1,5 +0,0 @@
-import "server-only"
-
-import Stripe from "stripe"
-
-export const stripe = new Stripe("sk_test_51SIk0l8vVGPqCtv3rtCom7QrR4biudIOKhgdXqXFllAFLp2O1PYQOyfd7dSdMchh3xz7DXHSFEuGqdY2ChE2S12900GpotfBjr")
